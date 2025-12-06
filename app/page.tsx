d02f2af9061d3ffe112a3d066bbcff13b46f8246
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-12-06 23:20:45 +0100

    test deployement

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 "use client";
 
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Features from "./components/Features";
-import HowItWorks from "./components/HowItWorks";
+// import About from "./components/About";
+// import Features from "./components/Features";
+// import HowItWorks from "./components/HowItWorks";
 // import Stats from "./components/Stats";
-import Services from "./components/Services";
-import Events from "./components/Events";
-import Testimonials from "./components/Testimonials";
-import Faq from "./components/Faq";
-import ContactUs from "./components/ContactUs";
+// import Services from "./components/Services";
+// import Events from "./components/Events";
+// import Testimonials from "./components/Testimonials";
+// import Faq from "./components/Faq";
+// import ContactUs from "./components/ContactUs";
 export default function Home() {
   return (
    <>
          <Hero />
-         <About/>
+         {/* <About/>
          <Features/>
          <HowItWorks/>
-         {/* <Stats/> */}
+         <Stats/>
          <Services/>
          <Events/>
          <Testimonials/>
          <Faq/>
-         <ContactUs/>
+         <ContactUs/> */}
    </>
   );
 }
