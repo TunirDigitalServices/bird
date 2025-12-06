fcf37099060e6124c4d731c8266122bcf345102e
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-12-06 23:24:06 +0100

    test deployement

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 // import About from "./components/About";
 // import Features from "./components/Features";
 // import HowItWorks from "./components/HowItWorks";
@@ -13,7 +13,8 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
    <>
-         <Hero />
+   hi
+         {/* <Hero /> */}
          {/* <About/>
          <Features/>
          <HowItWorks/>
