009f3ac1a3516acc90fed3c01c3dc0f8fa2000cf
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-12-06 23:10:55 +0100

    test deployement

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
          <About/>
          <Features/>
          <HowItWorks/>
-         <Stats/>
+         {/* <Stats/> */}
          <Services/>
          <Events/>
          <Testimonials/>
