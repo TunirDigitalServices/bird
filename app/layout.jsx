ba1c61be5d4741d24b93ad6630233400cd7e9e6b
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-12-06 23:37:42 +0100

    test deployement

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./template.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+"use client";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { useEffect } from "react";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function ClientLayout({ children }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
 
-export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`antialiased ${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }
