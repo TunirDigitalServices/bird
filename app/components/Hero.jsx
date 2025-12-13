feacc65be2a87b4162773a887b78d15a716285ed
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-12-13 13:39:49 +0100

    bootstrpa js

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -16,7 +16,7 @@ export default function Hero() {
                   data-aos="fade-up"
                   data-aos-delay="0"
                 >
-                Accompagnement humain & Innovation
+                Accompagnement humain et Innovation
                 </span>
 
                 <h1
